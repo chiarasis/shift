94a84f9717f377a0efaa37f9358f733cbe42a88d
chiarasis <chiarasisto03@gmail.com>
2025-01-29 15:02:11 +0100

    video

--- common.js
+++ common.js
@@ -2,16 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     let menuIcona = document.getElementById("menu-icona");
     let dropdownMenu = document.querySelector("ul.dropdown");
 
-    menuIcona.addEventListener("click", function () {
-        dropdownMenu.classList.toggle("active"); // Mostra/nasconde il menu
-        menuIcona.classList.toggle("active"); // Cambia icona
-    });
+    if (menuIcona && dropdownMenu) {
+        menuIcona.addEventListener("click", function () {
+            dropdownMenu.classList.toggle("active"); // Mostra/nasconde il menu
+            menuIcona.classList.toggle("active"); // Cambia icona
+        });
 
-    // Chiude il menu se si clicca fuori
-    document.addEventListener("click", function (event) {
-        if (!menuIcona.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.classList.remove("active");
-            menuIcona.classList.remove("active");
-        }
-    });
+        // Chiude il menu se si clicca fuori
+        document.addEventListener("click", function (event) {
+            if (!menuIcona.contains(event.target) && !dropdownMenu.contains(event.target)) {
+                dropdownMenu.classList.remove("active");
+                menuIcona.classList.remove("active");
+            }
+        });
+    }
 });
