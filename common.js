e8507c78ab86ebd1d4bbd4f55c8ae8953bbe9e0a
chiarasis <chiarasisto03@gmail.com>
2025-01-29 14:41:42 +0100

    menu

--- common.js
+++ common.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     menuIcona.addEventListener("click", function () {
         dropdownMenu.classList.toggle("active"); // Mostra/nasconde il menu
-        menuIcona.classList.toggle("active"); // Cambia l'icona
+        menuIcona.classList.toggle("active"); // Cambia icona
     });
 
-    // Chiude il menu cliccando fuori
+    // Chiude il menu se si clicca fuori
     document.addEventListener("click", function (event) {
         if (!menuIcona.contains(event.target) && !dropdownMenu.contains(event.target)) {
             dropdownMenu.classList.remove("active");
