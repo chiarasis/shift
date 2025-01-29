61e6d1a3bffbbed3ebfdb0f487c8f930601e142c
chiarasis <chiarasisto03@gmail.com>
2025-01-29 14:26:45 +0100

    common

--- common.js
+++ common.js
@@ -6,4 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
         dropdownMenu.classList.toggle("active"); // Mostra/nasconde il menu
         menuIcona.classList.toggle("active"); // Cambia l'icona
     });
+
+    // Chiude il menu cliccando fuori
+    document.addEventListener("click", function (event) {
+        if (!menuIcona.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.remove("active");
+            menuIcona.classList.remove("active");
+        }
+    });
 });
